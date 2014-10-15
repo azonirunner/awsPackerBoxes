32ba8617313ca3e38189936eace7b33ab75c30d7
azoniruner <mhickie@gmail.com>
2014-10-15 13:20:10 -0700

    updated the sed script to be correct

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,7 +6,7 @@ echo "Running Yum Update"
 sudo yum -y update
 
 echo "updating the bash_profile"
-sudo sed -i -e "\$a /\nalias ll='ls -al'\n" /home/ec2-user/.bash_profile
+sudo sed -i -e "\$a # ADDING PERSONALIZATION \nalias ll='ls -al'\n" /home/ec2-user/.bash_profile
 sudo sed -i -e "\$aset -o vi\n" /home/ec2-user/.bash_profile
 sudo sed -i -e "\$aexport HISTSIZE='5000'\n" /home/ec2-user/.bash_profile
 sudo sed -i -e "\$aexport HISTFILESIZE='10000'\n" /home/ec2-user/.bash_profile
